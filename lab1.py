37d211272d153f35ffbda8d31ab093da81b0dc5b
1Sl4M <shopalovis@gmail.com>
2023-01-23 22:12:49 +0600

    Add 'lab1.py' file

---
+++ lab1.py
@@ -0,0 +1,17 @@
+'''
+Многострочный код
+'''
+
+student = input('Введите имя: ')
+age = int(input('Введите возраст: '))
+if student != '':
+    print("You are student")
+elif student == '':
+    print("Who are you?")
+
+if age>18:
+    print("You are student")
+else:
+    print("You are schoolboy")
+
+
