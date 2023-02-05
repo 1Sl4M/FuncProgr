e9ed7a2167649212a900636b521b56b47bf7a017
1Sl4M <shopalovis@gmail.com>
2023-02-05 15:07:34 +0600

    Add file 'lab3.py'

---
+++ lab3.py
@@ -0,0 +1,86 @@
+from random import random, randrange, randint
+
+#1-st task
+
+# age = int(input())
+# year = age + 2003
+
+# if age > 21 and age < 17:
+#     print("You are not a student, cause your age is: ", age, ", year obucheniya: ", year)
+
+# while age < 21:
+#     print("You are a student, and your age is: ", age, ", year obucheniya: ", year)
+#     age = age + 1
+#     year = year + 1
+
+# print()
+
+# word = "hello"
+# for i in word:
+#     print(i);
+#     for j in range(1,5):
+#         print(word)
+        
+#2-nd task
+         
+# list = ["Islam", "Alihan", "Dimash", "Iliyas"]
+
+# for i in list:
+#     print(i)
+
+# a = []
+# for i in range(int(input("Input size of list "))):
+#     a.append((input()))
+# print(a)    
+
+# print()         
+         
+#3-rd task
+
+# ssg = randint(0, 10)
+# print(round(ssg))
+
+# ssg2 = randrange(50, 100, 2)
+# print(round(ssg2))
+
+# ssg3 = random()
+# print(round(ssg3))
+
+
+# l = ['one', 'two', 'three']
+# for i, val in enumerate(l):
+#     print(f'{i}: {val}')
+
+#4 task - 1
+
+# A = int(input("Input first number "))
+# B = int(input("Input second number "))
+
+# if A>B:
+#     print("First number have to be more than second")
+# else:
+#     for i in range(A, B+1):
+#         print(i)
+
+#4 task - 2
+
+# A = int(input("Input first number "))
+# B = int(input("Input second number "))
+
+# if A < B:
+#     for i in range(A, B+1):
+#         print(i)
+# else:
+#     for i in range(A, B-1, -1):
+#         print(i)
+
+#4 task - 3
+
+#A = int(input())
+#B = int(input())
+#for i in range(A - (A + 1) % 2, B - B % 2, -2):
+#    print(i, end=' ')
+
+
+
+
