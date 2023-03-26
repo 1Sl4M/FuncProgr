5783e5056ea50c7e0b6f921306960e21c557e8e5
1Sl4M <shopalovis@gmail.com>
2023-03-26 13:42:03 +0600

    AlikhanBranch

--- lab2.py
+++ lab2.py
@@ -7,11 +7,17 @@ age = int(input('Введите возраст: '))
 if student != '':
     print("You are student")
 elif student == '':
-    print("Who are you?")
+    print("are you monster?")
+    print("are you monster?")
+    print("are you monster?")
 
 if age>18:
     print("You are student")
+    print("You are student")
+    print("You are student")
 else:
+    print("You are schoolboy")
+    print("You are schoolboy")
     print("You are schoolboy")
 
 
